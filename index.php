17d733d9fb247d8d48176956c65ef5ca4e14454c
JITHU000000 <nalinishree33@gmail.com>
2025-12-02 15:51:23 +0530

    Point app to private DB via public EC2

--- index.php
+++ index.php
@@ -1,47 +1,4 @@
-<?php
-$host = '127.0.0.1';     // DB is on the same EC2
+$host = '10.0.3.16';      // private IP of DB EC2
 $user = 'appuser';
 $pass = 'AppPassword123!';
 $db   = 'appdb';
-
-$mysqli = new mysqli($host, $user, $pass, $db);
-
-if ($mysqli->connect_errno) {
-    echo "<h1>DB Connection Failed</h1>";
-    echo "<p>Error: " . $mysqli->connect_error . "</p>";
-    exit;
-}
-
-$result = $mysqli->query("SELECT NOW() AS now");
-$row = $result->fetch_assoc();
-
-echo "<h1>App → MySQL connection successful!</h1>";
-echo "<p>Time from DB: " . $row['now'] . "</p>";
-echo "<p>Served from host: " . gethostname() . "</p>";
-
-$mysqli->close();
-?>
-[ec2-user@ip-10-0-3-16 ~]$ sudo cat /var/www/html/index.php
-<?php
-$host = '127.0.0.1';     // DB is on the same EC2
-$user = 'appuser';
-$pass = 'AppPassword123!';
-$db   = 'appdb';
-
-$mysqli = new mysqli($host, $user, $pass, $db);
-
-if ($mysqli->connect_errno) {
-    echo "<h1>DB Connection Failed</h1>";
-    echo "<p>Error: " . $mysqli->connect_error . "</p>";
-    exit;
-}
-
-$result = $mysqli->query("SELECT NOW() AS now");
-$row = $result->fetch_assoc();
-
-echo "<h1>App → MySQL connection successful!</h1>";
-echo "<p>Time from DB: " . $row['now'] . "</p>";
-echo "<p>Served from host: " . gethostname() . "</p>";
-
-$mysqli->close();
-?>
